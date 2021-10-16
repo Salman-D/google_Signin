0079c6ffccb7968ffc8830eb2006223d786607d6
Salman-D <87569242+Salman-D@users.noreply.github.com>
2021-10-16 13:21:33 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+function gsignin(){
+    var  provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+    .then((res)=>{
+        console.log(res)
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+
+    console.log(provider)
+}
