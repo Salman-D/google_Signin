89482e9af08cd6f1097cbdf124db816f6d332fd8
Salman-D <87569242+Salman-D@users.noreply.github.com>
2021-10-16 13:38:40 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
+var img = document.getElementById('userimg')
+var email = document.getElementById("useremail")
+var Name = document.getElementById("userName")
+
+
+
 function gsignin(){
-    var  provider = new firebase.auth.GoogleAuthProvider();
+
+    var provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithPopup(provider)
     .then((res)=>{
-        console.log(res)
+        console.log(res.user)
+        console.log(res.user.email)
+        console.log(res.user.displayName)
+        console.log(res.user.photoURL)
+        
+        var user = res.user 
+        img.src=user.photoURL
+        email.innerText = user.email
+        Name.innerText = user.displayName
+        
+        
     })
     .catch((err)=>{
         console.log(err)
     })
 
     console.log(provider)
+
+
 }
